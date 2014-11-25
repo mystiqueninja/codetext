f01d4bdc7f5529d299b09da27741f6e74e9ed6ec
Mystique <tyrell@mystiquedesign.ml>
2014-11-25 09:48:15 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+"use strict"
+var myApp = angular.module('myApp', [
+	'ngRoute', 
+	'staffControllers'
+]);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+	.when('/editor', {
+		templateUrl: 'partials/editor.html',
+		controller: 'editorController'
+	})
+	.otherwise({
+		redirectTo: '/editor'
+	});
+}]);
